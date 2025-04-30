62397128f337bc56a428a3c2b59d7fa3c810bb1c
Manish Kumar <manishkumar80819@gmail.com>
2025-04-30 17:49:34 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Vite-E-commerce/",
-  // base: './', // ✅ This tells Vite to use relative asset paths
+  // base: "/Vite-E-commerce/",
+  base: "./",
 });
